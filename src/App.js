8162c103eea8e678a69f114f2e0f4dd8a51df14a
JeremyZXi <zjeremy0710@gmail.com>
2024-07-28 11:57:03 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const handleClick = () => {
+    setMessage('你好，世界！');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>简单的 React 应用程序</h1>
+          <button onClick={handleClick}>点击我</button>
+          {message && <p>{message}</p>}
+        </header>
+      </div>
   );
 }
 
